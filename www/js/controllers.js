518d4cd44b21a49f33d61f0c79d9f34879db4e47
Rayner Vintervoll <rayner.vintervoll@accenture.com>
2014-11-23 12:17:00 +0000

    Added logging

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', ['$scope', '$log', function($scope, $log) {
+  $log.debug('Loaded DashCtrl successfully.');
   $scope.region = {uuid:null, identifier:"DefaultRegion"};
   $scope.beacons = {};
-  var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region);
+  var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region).fail($log.error)
+				.done();
   cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion);
   var delegate = new cordova.plugins.locationManager.Delegate().implement({
 			didRangeBeaconsInRegion: function (pluginResult) {
+			  $log.debug('didRangeBeaconsInRegion()');
 			  $scope.beacons = pluginResult;
 			}
 		});
 	cordova.plugins.locationManager.setDelegate(delegate);
-})
+}])
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
