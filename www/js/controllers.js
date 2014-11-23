82737acaf6cd838e59787d09c00298da43e6b639
Rayner Vintervoll <rayner.vintervoll@accenture.com>
2014-11-23 12:44:34 +0000

    Delegate according to new pattern

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,19 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', ['$scope', '$log', function($scope, $log) {
-  $log.debug('Loaded DashCtrl successfully.');
+  $log.info('Loaded DashCtrl successfully.');
   $scope.region = {uuid:null, identifier:"DefaultRegion"};
   $scope.beacons = {};
   var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region).fail($log.error)
 				.done();
   cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion);
-  var delegate = new cordova.plugins.locationManager.Delegate().implement({
-			didRangeBeaconsInRegion: function (pluginResult) {
-			  $log.debug('didRangeBeaconsInRegion()');
+  var delegate = new cordova.plugins.locationManager.Delegate();
+  
+  delegate.didRangeBeaconsInRegion = function (pluginResult) {
+			  $log.info('didRangeBeaconsInRegion()');
 			  $scope.beacons = pluginResult;
-			}
-		});
+	};
+	
 	cordova.plugins.locationManager.setDelegate(delegate);
 }])
 
