343ce199131614f0ee45092a2396663ff763595d
Rayner Vintervoll <rayner.vintervoll@accenture.com>
2014-11-23 11:41:30 +0000

    Test petermetz

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Beacons) {
-  $scope.beacons = Beacons.all();
+.controller('DashCtrl', function($scope) {
+  $scope.region = {uuid:null, identifier:"DefaultRegion"};
+  $scope.beacons = {};
+  var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region);
+  cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion);
+  var delegate = new cordova.plugins.locationManager.Delegate().implement({
+			didRangeBeaconsInRegion: function (pluginResult) {
+			  $scope.beacons = pluginResult;
+			}
+		});
+	cordova.plugins.locationManager.setDelegate(delegate);
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
