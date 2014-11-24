80bd21f59a4318e54105c23108e7512f6333eca8
Rayner Vintervoll <rayner.vintervoll@accenture.com>
2014-11-24 10:29:11 +0000

    Waiting for ionicPlatform ready

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,21 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', ['$scope', '$log', function($scope, $log) {
-  $log.info('Loaded DashCtrl successfully.');
-  $scope.region = {uuid:null, identifier:"DefaultRegion"};
-  $scope.beacons = {};
-  var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region).fail($log.error)
-				.done();
-  cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion);
-  var delegate = new cordova.plugins.locationManager.Delegate();
-  
-  delegate.didRangeBeaconsInRegion = function (pluginResult) {
-			  $log.info('didRangeBeaconsInRegion()');
-			  $scope.beacons = pluginResult;
-	};
-	
-	cordova.plugins.locationManager.setDelegate(delegate);
-}])
+.controller('DashCtrl', function($scope, $console, $ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    $console.log('Loaded DashCtrl successfully.');
+    $scope.region = {
+      uuid: null,
+      identifier: "DefaultRegion"
+    };
+    $scope.beacons = {};
+    var beaconRegion = cordova.plugins.locationManager.Regions.fromJson($scope.region).fail($console.error)
+      .done();
+    cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion);
+    var delegate = new cordova.plugins.locationManager.Delegate();
+
+    delegate.didRangeBeaconsInRegion = function(pluginResult) {
+      $console.log('didRangeBeaconsInRegion()');
+      $scope.beacons = pluginResult;
+    };
+
+    cordova.plugins.locationManager.setDelegate(delegate);
+  });
+})
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
@@ -25,5 +30,4 @@ angular.module('starter.controllers', [])
   $scope.friend = Friends.get($stateParams.friendId);
 })
 
-.controller('AccountCtrl', function($scope) {
-});
+.controller('AccountCtrl', function($scope) {});
